73b819a07345ab47fc41d52a69a78d2ba27c79b3
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2026-06-20 15:34:59 +1200

    feat: add flash to nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,1 +1,53 @@
-return {}
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "<leader>eb",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            pattern = [[\<\k]],
+            search = {
+              mode = "search",
+              multi_window = false,
+            },
+            label = {
+              after = { 0, 0 },
+              before = false,
+            },
+            highlight = {
+              backdrop = true,
+              matches = false,
+            },
+          })
+        end,
+        desc = "Flash word begin",
+      },
+      {
+        "<leader>ee",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump({
+            pattern = [[\k\>]],
+            search = {
+              mode = "search",
+              multi_window = false,
+            },
+            label = {
+              after = { 0, 0 },
+              before = false,
+            },
+            highlight = {
+              backdrop = true,
+              matches = false,
+            },
+          })
+        end,
+        desc = "Flash word end",
+      },
+    },
+  },
+}
